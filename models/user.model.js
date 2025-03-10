cebf6f368c47b948d4867ec3c8049b239d810e3e
DebnathKaushik <debnathkaushik49@gmail.com>
2025-03-10 14:53:20 +0600

    distinct user and email And valid input

--- models/user.model.js
+++ models/user.model.js
@@ -7,19 +7,16 @@ const UserSchema = new mongoose.Schema({
         required:true,
         trim:true,
         lowercase:true,
-        minlength:[3,"Username must be at least 3 charecters"]
     },email:{
         type:String,
         unique:true,
         required:true,
         trim:true,
         lowercase:true,
-        minlength:[3,"email must be at least 3 charecters"]
     },password:{
         type:String,
         required:true,
         trim:true,
-        minlength:[3,"password must be at least 3 charecters"]
     }
 
 })
--- routes/user.route.js
+++ routes/user.route.js
@@ -8,28 +8,43 @@ const {body,validationResult} = require('express-validator') // 3rd Party middle
 router.get("/register",(req,res)=>{
     res.render("register")
 })
-router.post("/register", async (req,res,next)=>{
-    body('email').trim().isEmail().isLength({min:4}),
+
+//3rd Party middleware
+const bodyMiddleware =[
     body('username').trim().isLength({min:4}),
+    body('email').trim().isEmail().isLength({min:4}),
     body('password').trim().isLength({min:4})
+]
+
+router.post("/register",bodyMiddleware ,async (req,res)=>{
 
     const errors = validationResult(req)
-    if(!errors.isEmpty()){
-        res.send(errors)
+        if(!errors.isEmpty()){
+            return res.status(400).json({errors:errors.array(),message:"Invalid"})
+        }else{   
+        const { username, email, password } = req.body;
+        try {
+            // Check if the email and username already exists in the database
+            const[existingemail,existingUser] = await Promise.all([
+                UserModel.findOne({email}),
+                UserModel.findOne({username})
+            ])
+            if (existingUser || existingemail) {
+                return res.status(400).json({ message: "Email  Or Username already in use" });
+            }else{
+            // Create a new user if email is unique
+            const newUser = await UserModel.create({username,email,password});
+                return res.status(201).json(newUser);
+            }
+        
+        } catch (error) {
+            // Handle any unexpected errors
+            console.error(error);
+            return res.status(500).json({ message: "Internal Server Error " });
+        }
     }
-        
-    const {username,email,password} = req.body
+});
 
-        const newuser = await UserModel.create({
-            "username":username,
-            "email":email,
-            "password":password
-        })
-        
-    next(newuser)
-},(req,res)=>{
-    res.json(newuser)
-})
 
 
 
