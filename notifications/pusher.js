a4dfd6e40d719ea4e820600509faf15068a20d94
DebnathKaushik <debnathkaushik49@gmail.com>
2025-05-30 17:27:29 +0600

    new cluster app in pusher(DriveApp)

--- notifications/pusher.js
+++ notifications/pusher.js
@@ -1,9 +1,9 @@
 const Pusher = require('pusher');
 
 const pusher = new Pusher({
-  appId: "2000756",
-  key: "f29922464eb3c0945eb6",
-  secret: "7924841f3aff08bc0cb5",
+  appId: "2000938",
+  key: "62ee3b1c9692423af804",
+  secret: "66c64e270c653b7ae55d",
   cluster: "ap1",
   useTLS: true
 });
